ba4d9bc9cebf6acc6e6e9153bb3789f12ce17d4f
Jbrod <jorgebravo202131782@cunoc.edu.gt>
2024-10-18 18:12:08 -0600

    Componente: div

--- GeneradorCaptchas/src/app/componentes/Body.js
+++ GeneradorCaptchas/src/app/componentes/Body.js
@@ -14,11 +14,46 @@ export class Body extends Componente{
         switch(parametro){
             case 'BACKGROUND': 
                 if(this.background == undefined){
-                    this.background = valor;
+                    c = valor.toLowerCase();
+                    switch(c){
+                        case 'black': this.background = '#000000';
+                            break;
+                        case 'olive': this.background =  '#808000';
+                            break;
+                        case 'teal': this.background = '#008080';
+                            break; 
+                        case 'red': this.background = '#FF0000';
+                            break; 
+                        case 'blue': this.background = '#0000FF';
+                            break;
+                        case 'maroon': this.background = '#800000';
+                            break; 
+                        case 'navy': this.background = '#000080';
+                            break; 
+                        case 'gray': this.background = '#808080';
+                            break; 
+                        case 'lime': this.background = '#00FF00';
+                            break; 
+                        case 'fucshia': this.background = '#FF00FF';
+                            break; 
+                        case 'green': this.background = '#008000';
+                            break; 
+                        case 'white': this.background = '#FFFFFF';
+                            break; 
+                        case 'purple': this.background = '#800080';
+                            break;
+                        case 'silver': this.background = '#C0C0C0';
+                            break;
+                        case 'yellow': this.background = '#FFFF00';
+                            break; 
+                        case 'aqua': this.background = '#00FFFF';
+                            break;
+                        default: 
+                            this.background = valor;  
+                    }
                 }else{
                     mensaje = "Error Semantico: se intento establecer el parametro background pero ya estaba definido.";
-                    console.log(mensaje);
-                    throw new Error(mensaje);
+                    super.lanzarExcepcionSemantica(error);
                 }
                 break;
             default:
@@ -32,7 +67,7 @@ export class Body extends Componente{
      */
     obtenerCodigo(){
         if(this.background == undefined){
-            this.background = "white";
+            this.background = "#FFFFFF";
         }
         codigo = "<body style=\"background-color: " + this.background + ";\"></body>";
         return codigo; 
